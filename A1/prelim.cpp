cef4827719e520bb72b7ded20a127b85a9f83f4c
Aryan Gupta <kuantum.freak@gmail.com>
2019-01-14 13:48:30 -0500

    Update prelim code

--- A1/prelim.cpp
+++ A1/prelim.cpp
@@ -1,4 +1,22 @@
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <memory>
+
 int main () {
+  constexpr size_t CHAR_LEN = 1024;
+
+  std::unique_ptr<char> str{ new char[CHAR_LEN] };
+  int ret = gethostname(str.get(), CHAR_LEN);
+
+  if (ret != 0) {
+    std::cout << "Somthing went wrong" << std::endl;
+    return -1;
+  }
+
+  std::ofstream file{ "preliminary_answer" };
+  file << str.get() << std::endl;
+  file.close();
 
   return 0;
 }
