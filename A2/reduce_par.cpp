89817eed28705386913e6c89b45460e91c09ed32
Aryan Gupta <kuantum.freak@gmail.com>
2019-02-10 16:08:12 -0500

    Update reduce parallel function

--- A2/reduce_par.cpp
+++ A2/reduce_par.cpp
@@ -4,9 +4,14 @@
 #include <vector>
 #include <thread>
 #include <cassert>
+// #include <numeric>
+// #include <execution>
+#include <parallel/numeric>
 
-constexpr unsigned long long LG_NUM = 200'000'000;
+constexpr unsigned long long LG_NUM = 500'000'007;
 
+/// @warning I know there wont be any race conditions on `data`
+///          variable because it is unique to each thread.
 template <typename T>
 void par_sum (T* start, T* end, T* dest) {
 	*dest = *start;
@@ -18,31 +23,10 @@ void par_sum (T* start, T* end, T* dest) {
 template <typename T>
 T reduce_sin(T* array, size_t n, size_t p = 1) {
 	assert(p == 1);
-
-	// create nessicary structures
-	std::unique_ptr<T[]> data{ new T[p] };
-
-	// calculate parallelism
-	size_t depth = n / p;
-	T* start = array;
-	T* end = array + depth;
-
-	// the actual parallel code
-	for (size_t i = 0; i < p - 1; ++i) {
-		par_sum(start, end, &data[i]);
-		start = end;
-		end += depth;
-	}
-
-	// the last one
-	par_sum(start, array + n, &data[p - 1]);
-
-	// final sum
-	T sum{  };
-	for (size_t i = 0; i < p; ++i)
-		sum += data[i];
-
-	return sum;
+	T result = array[0];
+	for (int i=1; i<n; ++i)
+		result = [](auto a, auto b) { return a + b; }(result, array[i]);
+	return result;
 }
 
 template <typename T>
@@ -65,8 +49,8 @@ T reduce_par (T* array, size_t n, size_t p) {
 		end += depth;
 	}
 
-	// the last one
-	threads.emplace_back(par_sum<T>, start, array + n, &data[p - 1]);
+	// the last one (this thread will do it so we dont spin on waiting for them to join)
+	par_sum(start, array + n, &data[p - 1]);
 
 	// wait for them to finish
 	for (auto& t : threads)
@@ -82,9 +66,9 @@ T reduce_par (T* array, size_t n, size_t p) {
 
 
 int main() {
-	std::vector<unsigned long long> sum{};
+	std::vector<int> sum{};
 	for (unsigned long long i = 1; i < LG_NUM + 1; ++i) {
-		sum.push_back(i);
+		sum.push_back(1); // sums of ones
 	}
 
 	auto begin1  = std::chrono::high_resolution_clock::now(); //// AHHHHHHH
@@ -101,7 +85,25 @@ int main() {
 	std::chrono::duration<double, std::milli> elapse2{ end2 - begin2 };
 	std::cout << "Parallel: " << elapse2.count() << std::endl;
 
-	if (accum1 == accum2)
+	// Turns out that neither gnu g++ or clang++ supports the parallelism TS, kms
+	// https://youtu.be/Mcjrc2uxbKI?t=571 MSVC ahead of the game *slow claps*
+	// nvm: https://godbolt.org/z/TCGaze The internet video lies
+	// auto begin3 = std::chrono::high_resolution_clock::now();
+	// double result = std::reduce(std::execution::par, sum.begin(), sum.end());
+	// auto end3 = std::chrono::high_resolution_clock::now();
+
+	// std::chrono::duration<double, std::milli> elapse3{ end3 - begin3 };
+	// std::cout << "STL: " << elapse3.count();
+
+	// hehe https://gcc.gnu.org/onlinedocs/libstdc++/manual/parallel_mode.html
+	auto begin4 = std::chrono::high_resolution_clock::now();
+	double accum4 = __gnu_parallel::accumulate(sum.begin(), sum.end(), 0);
+	auto end4 = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double, std::milli> elapse4{ end4 - begin4 };
+	std::cout << "GNU: " << elapse4.count() << std::endl;
+
+	if (accum1 == accum2 and accum2 == accum4)
 		std::cout << "The sum is: " << accum1 << std::endl;
 	else
 		std::cout << "The sums were different, you screwed up..." << std::endl;
