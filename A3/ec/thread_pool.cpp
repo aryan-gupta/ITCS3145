a70d4ec0ba88ead7b7445699d55b168454b47330
Aryan Gupta <kuantum.freak@gmail.com>
2019-02-13 10:53:14 -0500

    Add base file for thread_pool

---
+++ A3/ec/thread_pool.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <chrono>
+#include <cmath>
+#include <chrono>
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+
+#include <pthread.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+int main(int argc, char* argv[]) {
+
+}
