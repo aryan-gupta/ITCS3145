04098375ceaba6b4e2daf42a33f85e6b99fd8024
Aryan Gupta <kuantum.freak@gmail.com>
2019-02-27 20:28:10 -0500

    Finish reduce code

--- A4/reduce/reduce.cpp
+++ A4/reduce/reduce.cpp
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <chrono>
+using clk = std::chrono::high_resolution_clock;
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,20 +31,50 @@ int main (int argc, char* argv[]) {
       std::cerr<<"something is amiss"<<std::endl;
     }
   }
-  
+
   if (argc < 5) {
     std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads> <scheduling> <granularity>"<<std::endl;
     return -1;
   }
 
   int n = atoi(argv[1]);
+  int nbthreads = atoi(argv[2]);
+  omp_set_num_threads(nbthreads);
+
+  int gran = atoi(argv[4]);
+  switch (argv[3][0]) {
+    case 's': omp_set_schedule( omp_sched_static, gran); break;
+    case 'd': omp_set_schedule(omp_sched_dynamic, gran); break;
+    case 'g': omp_set_schedule( omp_sched_guided, gran); break;
+    default : std::cout << "Hold up, what just happened...." << std::endl; return EXIT_FAILURE;
+  }
+
   int * arr = new int [n];
 
   generateReduceData (arr, atoi(argv[1]));
 
-  //insert reduction code here
-  
-  
+  auto tStart = clk::now();
+  int sum{  };
+
+  #pragma omp parallel
+  {
+    int psum{  };
+
+    #pragma omp for
+    for (size_t i = 0; i < n; ++i) {
+      psum += arr[i];
+    }
+
+    #pragma omp critical
+    sum += psum;
+  }
+
+  auto tEnd = clk::now();
+  std::chrono::duration<double> elapse = tEnd - tStart;
+
+  std::cout << sum << std::endl;
+  std::cerr << elapse.count() << std::endl;
+
   delete[] arr;
 
   return 0;
