bdca36d19d83b3760fce66a3bd03b9b24e387cdf
Aryan Gupta <kuantum.freak@gmail.com>
2019-03-09 20:37:07 -0500

    Add merge sort merge also from A3-ec

--- A4/mergesort/mergesort.cpp
+++ A4/mergesort/mergesort.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <unistd.h>
 
+#include <vector>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,6 +15,29 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+namespace detail {
+// Merge Sort merge
+template <typename I, typename O>
+void merge_sort_merge(I begin, I mid, I end, O op) {
+	std::vector<typename std::iterator_traits<I>::value_type> tmp{  };
+
+  auto lbegin = begin;
+	auto rbegin = mid;
+
+	while (lbegin != mid and rbegin != end) {
+		if (op(*lbegin, *rbegin))
+			tmp.push_back(std::move(*lbegin++));
+		else
+			tmp.push_back(std::move(*rbegin++));
+	}
+
+	begin = std::move(tmp.begin(), tmp.end(), begin);
+
+	begin = std::move(begin, mid, begin);
+	std::move(rbegin, end, begin);
+}
+}
 
 
 int main (int argc, char* argv[]) {
@@ -28,13 +53,13 @@ int main (int argc, char* argv[]) {
       std::cerr<<"something is amiss"<<std::endl;
     }
   }
-  
+
   if (argc < 3) { std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
     return -1;
   }
 
   int n = atoi(argv[1]);
-  
+
   // get arr data
   int * arr = new int [n];
   generateMergeSortData (arr, n);
@@ -42,9 +67,9 @@ int main (int argc, char* argv[]) {
   //insert sorting code here.
 
 
-  
+
   checkMergeSortResult (arr, n);
-  
+
   delete[] arr;
 
   return 0;
