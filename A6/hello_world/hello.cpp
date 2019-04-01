7733a13c1e18f1b8ad75f94c9ef97714411d5974
Aryan Gupta <kuantum.freak@gmail.com>
2019-04-01 13:37:06 -0400

    Finish code for mamba push

--- A6/hello_world/hello.cpp
+++ A6/hello_world/hello.cpp
@@ -2,6 +2,19 @@
 #include <unistd.h>
 #include <iostream>
 
+#include <limits.h>
+#include <iostream>
+
 int main(int argc, char*argv[]) {
 
+	int rank{  }, size{  };
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	char str[HOST_NAME_MAX + 1];
+	int ret = gethostname(str, HOST_NAME_MAX + 1);
+
+	std::cout << "I am process "<< rank << " out of " << size << " . I am running on " << str << "." << std::endl;
 }
