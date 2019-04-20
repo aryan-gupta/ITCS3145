ec6697c891d0c9732f517513bd1c6ba974d91b24
Aryan Gupta <kuantum.freak@gmail.com>
2019-04-20 16:59:32 -0400

    Fix parter rank issue

--- A7/pingpong/mpi_ping_pong.cpp
+++ A7/pingpong/mpi_ping_pong.cpp
@@ -18,12 +18,12 @@ int main (int argc, char* argv[]) {
   if (rank == 0) {
     num = std::atoi(argv[1]); // read value
     MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // send to process 1
-    MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL); // get back number (num + 2)
+    MPI_Recv(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, NULL); // get back number (num + 2)
     std::cout << num << std::endl; // print the number
   } else {
     MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, NULL); // recive number
     num += 2; // add 2
-    MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // send back number
+    MPI_Send(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); // send back number
   }
 
 	MPI_Finalize();
