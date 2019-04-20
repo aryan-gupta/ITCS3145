ddb6ed30abb312a7bf742a50130539aa7836df81
Aryan Gupta <kuantum.freak@gmail.com>
2019-04-20 16:32:16 -0400

    Add code for ping pong

--- A7/pingpong/mpi_ping_pong.cpp
+++ A7/pingpong/mpi_ping_pong.cpp
@@ -8,8 +8,25 @@ int main (int argc, char* argv[]) {
     return -1;
   }
 
-  
+	MPI_Init(&argc, &argv);
 
+  int rank{  }, size{  };
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  int num;
+  if (rank == 0) {
+    num = std::atoi(argv[1]); // read value
+    MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // send to process 1
+    MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // get back number (num + 2)
+    std::cout << num << std::endl; // print the number
+  } else {
+    MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // recive number
+    num += 2; // add 2
+    MPI_Send(&num, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // send back number
+  }
+
+	MPI_Finalize();
 
   return 0;
 }
